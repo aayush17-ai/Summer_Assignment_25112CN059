70a09e227f0c2a56a310a0074a4141503e8db55c
unknown <you@example.com>
2026-06-03 19:55:06 +0530

    day-3 assignment uploaded

---
+++ day-3/prime_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+int n;
+printf("Enter the number n-");
+scanf("%d",&n);
+
+for(int i=2;i<n;i++){
+
+if(n%i==0){
+printf("The number is not prime");
+
+return 0;}}
+
+printf("The number is prime");
+
+}
