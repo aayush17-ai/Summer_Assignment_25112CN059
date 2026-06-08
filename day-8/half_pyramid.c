42f67853a2e2e1847914a8db09ec6cd59b8dc5d6
unknown <you@example.com>
2026-06-08 21:51:10 +0530

    day-8 assignment uploaded

---
+++ day-8/half_pyramid.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int i,row;
+
+printf("Enter the number of rows:");
+scanf("%d",&row);
+
+for(i=1;i<=row;i++){
+    for(int j=1;j<=i;j++){
+
+        printf(" * ");
+    }
+    printf("\n");
+}
+return 0;
+}
