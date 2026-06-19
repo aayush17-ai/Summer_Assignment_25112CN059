9e479b5bc0fceb591f259dd4bf26c53929d66ce9
unknown <you@example.com>
2026-06-19 11:15:22 +0530

    day-19 assignment uploaded

---
+++ day-19/transpose_matrix.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int A[10][10], Transpose[10][10];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            Transpose[j][i] = A[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for(int i = 0; i < cols; i++) {
+        for(int j = 0; j < rows; j++) {
+            printf("%d ", Transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
